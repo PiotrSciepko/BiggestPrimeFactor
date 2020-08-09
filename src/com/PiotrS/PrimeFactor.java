e8809851365b6a39d8dd9f543b381b7be38a35e2
piotrs <piotrek.priv@gmail.com>
2020-08-09 19:12:05 +0200

    add isPrime() getMaxPrimeFactors()

--- src/com/PiotrS/PrimeFactor.java
+++ src/com/PiotrS/PrimeFactor.java
@@ -1,33 +1,64 @@
 package com.PiotrS;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class PrimeFactor {
-    int initialNumber;
+    final int initialNumber;
 
     public PrimeFactor(int initialNumber) {
         this.initialNumber = initialNumber;
     }
 
     public List<Integer> getPrimeFactors() {
-        List<Integer> primeFactors = new ArrayList<Integer>();
-
+        List<Integer> primeFactors = new ArrayList<>();
         int div = 2;
-        while (initialNumber > 1) {
-            while (initialNumber % div == 0) {
+        int n = initialNumber;
+        while (n > 1) {
+            while (n % div == 0) {
                 primeFactors.add(div);
-                initialNumber /= div;
+                n /= div;
             }
-            div++;
+            //div++;
+            div += (div == 2) ? 1 : 2;
         }
         return primeFactors;
     }
 
+    public List<Integer> getMaxPrimeFactors(List<Integer> list) {
+        List<Integer> maxPrimesFirst = new ArrayList<>();
+        int n = initialNumber;
+        int i = list.size() - 1;
+        int div;
+        while (n > 1) {
+            div = list.get(i);
+            while (n % div == 0) {
+                maxPrimesFirst.add(div);
+                n /= div;
+            }
+            i--;
+        }
+        return maxPrimesFirst;
+    }
+
+    public boolean isPrime() {
+        return getPrimeFactors().size() == 1;
+    }
+
     public static void main(String[] args) {
 
-        PrimeFactor p = new PrimeFactor(13);
-        System.out.println(p.getPrimeFactors());
+        PrimeFactor p = new PrimeFactor(256);
+
+        List<Integer> primeFactors = p.getPrimeFactors();
+        System.out.println(primeFactors);
+
+        System.out.println(p.getMaxPrimeFactors(primeFactors));
+
+        Collections.reverse(primeFactors);
+        System.out.println(primeFactors);
+
+        System.out.println(p.isPrime());
 
     }
 }
